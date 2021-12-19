bdc88881bbc23df5716a3255381f536be7566e15
Scott Freeland <90366840+scottfreeland@users.noreply.github.com>
2021-12-19 02:31:32 -0500

    Updated logic.js file.

--- Mapping_Multiple_Points/static/js/logic.js
+++ Mapping_Multiple_Points/static/js/logic.js
@@ -2,13 +2,8 @@
 console.log("working");
 
 // Create the map object with a center and zoom level.
-let map = L.map('mapid').setView([34.0522, -118.2437], 14);
+let map = L.map('mapid').setView([40.7, -94.5], 4);
 
-// Loop through the cities array and create one marker for each city.
-cities.forEach(function(city) {
-    console.log(city)
-   });
-   
 // An array containing each city's location, state, and population.
 let cities = [{
     location: [40.7128, -74.0059],
@@ -41,12 +36,14 @@ let cities = [{
     population: 1660272
   }
   ];
-
-
-
+  
+// Loop through the cities array and create one marker for each city.
+cities.forEach(function(city) {
+    console.log(city)
+   });
 
 // We create the tile layer that will be the background of our map.
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
 attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     accessToken: API_KEY
